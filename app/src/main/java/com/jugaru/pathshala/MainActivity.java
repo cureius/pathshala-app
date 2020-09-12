abd76a6e37790d45f6ac37d65d25abae1d99dd2e
sourajpal99 <paulsouraj99@gmail.com>
2020-09-12 22:55:38 +0530

    phase-3 task-7 commit-3

--- app/src/main/java/com/jugaru/pathshala/MainActivity.java
+++ app/src/main/java/com/jugaru/pathshala/MainActivity.java
@@ -13,6 +13,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
@@ -40,12 +45,23 @@ public class MainActivity extends AppCompatActivity {
     private FrameLayout frameLayout;
     private TabLayout tabLayout;
     private List<Fragment> fragmentList;
+    private AdView mAdView;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        MobileAds.initialize(this, "ca-app-pub-7613539201061048~6033111687");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
 
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
